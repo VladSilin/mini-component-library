d8fac5e6e6862810260e8fde33bd6aa52a91d6da
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-14 22:48:07 -0400

    Go through solution and make changes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,130 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const MAX_WIDTH = 370;
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <BorderWrapper>
+        <Bar
+          style={{
+            "--width": value + "%",
+            "--height": styles.height + "px",
+          }}
+        />
+      </BorderWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  padding: var(--padding);
+  width: ${MAX_WIDTH}px;
+
+  /* Trim off corners when near full */
+  overflow: hidden;
+`;
+
+const BorderWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
+
+//const Fill = styled.div`
+//  width: ${(p) => (p.value / 100) * MAX_WIDTH}px;
+//
+//  background-color: ${COLORS.primary};
+//`;
+//
+//const WrapperSmall = styled(Wrapper)`
+//  height: 8px;
+//  border-radius: 3px;
+//`;
+//const FillSmall = styled(Fill)`
+//  height: 8px;
+//`;
+//
+//const WrapperMedium = styled(Wrapper)`
+//  height: 12px;
+//  border-radius: 4px;
+//`;
+//const FillMedium = styled(Fill)`
+//  height: 12px;
+//`;
+//
+//const WrapperLarge = styled(Wrapper)`
+//  position: relative;
+//  height: 24px;
+//  border-radius: 8px;
+//`;
+//
+//const BorderWrapper = styled.div`
+//  --border-size: 4px;
+//
+//  position: relative;
+//  top: var(--border-size);
+//  left: var(--border-size);
+//  height: 16px;
+//  width: ${MAX_WIDTH - 8}px;
+//  border-radius: 4px;
+//  overflow: hidden;
+//`;
+//
+//const FillLarge = styled(Fill)`
+//  position: relative;
+//  top: 0;
+//  left: 0;
+//  height: 24px;
+//`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,92 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+import VisuallyHidden from "../VisuallyHidden";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <Wrapper>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <NativeSelect value={value} onChange={onChange}>
+          {children}
+        </NativeSelect>
+        <Presentational>
+          {displayedValue}
+          <ChevronWrapper style={{ "--size": 24 + "px" }}>
+            <Icon id="chevron-down" size={24} strokeWidth={2} />
+          </ChevronWrapper>
+        </Presentational>
+      </Wrapper>
+    </>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  -webkit-appearance: none;
+`;
+
+const Presentational = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const ChevronWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
+
+// TODO: Left off at min-width
+//const NativeSelect = styled.select`
+//  overflow: hidden;
+//  -webkit-appearance: none;
+//  appearance: none;
+//  outline: none;
+//
+//  background-color: inherit;
+//  color: ${COLORS.gray500};
+//  font-size: 0.75rem;
+//
+//  min-width: fit-content;
+//
+//  border: 0px;
+//  margin-right: 24px;
+//`;
 
 export default Select;
